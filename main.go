61620042683e9a5ea527e6f962feafc106e04b00
Diego Hernandez <diego.hdz6263@gmail.com>
2025-11-08 09:56:55 -0800

    update

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	"github.com/diegofhdz/unifi-log-streamer/client"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Creating UniFi Client")
+	num := 10
+	var ptr *int = &num // Declare a pointer to an integer
 
-	c, err := client.New("SomeUrl.Something")
+	fmt.Println("Value of num:", num)             // Output: Value of num: 10
+	fmt.Println("Address of num:", &num)          // Output: Address of num: 0xc000014088 (example address)
+	fmt.Println("Value of ptr:", ptr)             // Output: Value of ptr: 0xc000014088 (example address)
+	fmt.Println("Value pointed to by ptr:", *ptr) // Output: Value pointed to by ptr: 10
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	*ptr = 20 // Change the value at the address pointed to by 'ptr'
 
-	fmt.Printf("Successfully created client at ip %s", c.BaseUrl)
+	fmt.Println("New value of num:", num) // Output: New value of num: 20
+
+	num = 30
+	fmt.Println("Num", num)
 }
